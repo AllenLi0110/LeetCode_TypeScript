b2e21b2e4ed2c5b3cbba1847d4791b761ddf93fc
AllenLiDeveloper <allen.li.service@gmail.com>
2025-02-18 22:55:40 +0800

    feat: add 0005-longest-palindromic-substring.ts

---
+++ 0005-longest-palindromic-substring.ts
@@ -0,0 +1,25 @@
+function longestPalindrome(s: string): string {
+  if (s.length < 1) return "";
+  let start: number = 0;
+  let maxLength: number = 1;
+
+  const expandFromCenter = (left: number, right: number): number => {
+    while (left >= 0 && right < s.length && s[left] === s[right]) {
+      left--;
+      right++;
+    }
+    return right - left - 1;
+  };
+
+  for (let i = 0; i < s.length; i++) {
+    let len1: number = expandFromCenter(i, i);
+    let len2: number = expandFromCenter(i, i + 1);
+    let currentMaxLength = Math.max(len1, len2);
+
+    if (currentMaxLength > maxLength) {
+      maxLength = currentMaxLength;
+      start = i - Math.floor((currentMaxLength - 1) / 2);
+    }
+  }
+  return s.substring(start, start + maxLength);
+}
