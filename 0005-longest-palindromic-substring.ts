2d4d22d0ec2dd770e3e70a608b6ae0ad5a4a80b1
AllenliIE <allen.li.service@gmail.com>
2025-02-22 20:03:10 +0800

    feat: add solution

--- 0005-longest-palindromic-substring.ts
+++ 0005-longest-palindromic-substring.ts
@@ -1,6 +1,12 @@
 //Blog:http://allenliservice.online/leetcode-js-5-longest-palindromic-substring/
 
 // <strong>solution:</strong>
+// 1. 判斷 s 為空時，回傳 ""。
+// 2. 定義 start 為 0，maxLength 為 1。
+// 3. 建立擴展中心的函式，從字串中心來查找回文子串長度。
+// 4. 遍歷每個字串，並且分別檢查奇數和偶數長度的情況。
+// 5. 遍歷過程中不斷更新最長的字串長度。
+// 6. 最終返回最長的字串長度。
 
 // <strong>Code 1: BigO(n^2) Sliding Window</strong>
 https: function longestPalindrome(s: string): string {
