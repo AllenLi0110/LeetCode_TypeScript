1dd85fa6c14076b31a7167388a1c3219c6ffad10
AllenLiDeveloper <allen.li.service@gmail.com>
2025-10-15 22:31:09 +0800

    feat: formated

--- 1004-max-consecutive-ones-iii.ts
+++ 1004-max-consecutive-ones-iii.ts
@@ -32,17 +32,17 @@ function longestOnes(nums: number[], k: number): number {
 <pre style='background-color:#ggg'>
 Input: nums = [1,1,1,0,0,0,1,1,1,1,0], k = 2
 
-right=0, left=0, zeros=0, maxLength=1
-right=1, left=0, zeros=0, maxLength=2
-right=2, left=0, zeros=0, maxLength=3
-right=3, left=0, zeros=1, maxLength=4
-right=4, left=0, zeros=2, maxLength=5
-right=5, left=4, zeros=2, maxLength=5
-right=6, left=4, zeros=2, maxLength=5
-right=7, left=4, zeros=2, maxLength=5
-right=8, left=4, zeros=2, maxLength=5
-right=9, left=4, zeros=2, maxLength=6
-right=10, left=5, zeros=2, maxLength=6
+right = 0, left = 0, zeros = 0, maxLength = 1
+right = 1, left = 0, zeros = 0, maxLength = 2
+right = 2, left = 0, zeros = 0, maxLength = 3
+right = 3, left = 0, zeros = 1, maxLength = 4
+right = 4, left = 0, zeros = 2, maxLength = 5
+right = 5, left = 4, zeros = 2, maxLength = 5
+right = 6, left = 4, zeros = 2, maxLength = 5 
+right = 7, left = 4, zeros = 2, maxLength = 5
+right = 8, left = 4, zeros = 2, maxLength = 5
+right = 9, left = 4, zeros = 2, maxLength = 6
+right = 10, left = 5, zeros = 2, maxLength = 6
 return maxLength; // 6;
 </pre>
 
@@ -50,24 +50,24 @@ return maxLength; // 6;
 <pre style='background-color:#ggg'>
 Input: nums = [0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1], k = 3
 
-right=0, left=0, zeros=1, maxLength=1
-right=1, left=0, zeros=2, maxLength=2
-right=2, left=0, zeros=2, maxLength=3
-right=3, left=0, zeros=2, maxLength=4
-right=4, left=0, zeros=3, maxLength=5
-right=5, left=1, zeros=3, maxLength=5
-right=6, left=1, zeros=3, maxLength=6
-right=7, left=1, zeros=3, maxLength=7
-right=8, left=1, zeros=3, maxLength=8
-right=9, left=2, zeros=3, maxLength=8
-right=10, left=2, zeros=3, maxLength=9
-right=11, left=2, zeros=3, maxLength=10
-right=12, left=5, zeros=3, maxLength=10
-right=13, left=6, zeros=3, maxLength=10
-right=14, left=10, zeros=3, maxLength=10
-right=15, left=10, zeros=3, maxLength=10
-right=16, left=10, zeros=3, maxLength=10
-right=17, left=10, zeros=3, maxLength=10
-right=18, left=10, zeros=3, maxLength=10
+right = 0, left = 0, zeros = 1, maxLength = 1
+right = 1, left = 0, zeros = 2, maxLength = 2
+right = 2, left = 0, zeros = 2, maxLength = 3
+right = 3, left = 0, zeros = 2, maxLength = 4
+right = 4, left = 0, zeros = 3, maxLength = 5
+right = 5, left = 1, zeros = 3, maxLength = 5
+right = 6, left = 1, zeros = 3, maxLength = 6
+right = 7, left = 1, zeros = 3, maxLength = 7
+right = 8, left = 1, zeros = 3, maxLength = 8
+right = 9, left = 2, zeros = 3, maxLength = 8
+right = 10, left = 2, zeros = 3, maxLength = 9
+right = 11, left = 2, zeros = 3, maxLength = 10
+right = 12, left = 5, zeros = 3, maxLength = 10
+right = 13, left = 6, zeros = 3, maxLength = 10
+right = 14, left = 10, zeros = 3, maxLength = 10
+right = 15, left = 10, zeros = 3, maxLength = 10
+right = 16, left = 10, zeros = 3, maxLength = 10
+right = 17, left = 10, zeros = 3, maxLength = 10
+right = 18, left = 10, zeros = 3, maxLength = 10
 return maxLength; // 10;
 </pre> */
