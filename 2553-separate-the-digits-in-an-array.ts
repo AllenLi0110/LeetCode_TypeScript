1b160c5bf48f3bba471a3696925f5571252ceaa6
AllenLiDeveloper <allen.li.service@gmail.com>
2025-10-19 16:14:45 +0800

    feat: add 2553-separate-the-digits-in-an-array.ts

--- 2553-separate-the-digits-in-an-array.ts
+++ 2553-separate-the-digits-in-an-array.ts
@@ -1,13 +1,40 @@
+//Blog: https://allenliservice.online/leetcode-ts-2553-separate-the-digits-in-an-array/
+
+// <strong>Solution: </strong>
+// 這題給了我們一個數值，並且把每個數字都獨立並放入陣列中。
+// 所以我們可以遍歷這個 nums 陣列，
+// 並且把得到的數值轉成字串後放入到 arr 陣列中即可。
+
+// <strong>Code 1: BigO(n x k)</strong>
 function separateDigits(nums: number[]): number[] {
-  let arr: number[] = [];
-  let digit: number = 0;
-  for (const num of nums) {
-      let s: string = String(num);
-      while (s.length > 0) {
-          digit = Number(s.split('').shift());
-          arr.push(digit);
-          s = s.slice(1);
-      }        
-  }
-  return arr;
+    let arr: number[] = [];
+    for (const num of nums) {
+        for (const char of num.toString()) {
+            arr.push(Number(char));
+        }
+    }
+    return arr;
 };
+
+/* <strong>FlowChart:</strong>
+<strong>Example 1</strong>
+<pre style='background-color:#ggg'>
+Input: nums = [13,25,83,77]
+
+num 13, char 1, char 3
+num 25, char 2, char 5
+num 83, char 8, char 3
+num 77, char 7, char 7
+return [1,3,2,5,8,3,7,7]
+</pre>
+
+<strong>Example 2</strong>
+<pre style='background-color:#ggg'>
+Input: nums = [7,1,3,9]
+
+num 13, char 1, char 3
+num 25, char 2, char 5
+num 83, char 8, char 3
+num 77, char 7, char 7
+return [1,3,2,5,8,3,7,7]
+</pre> */
--- README.md
+++ README.md
@@ -81,6 +81,7 @@ The main files and their functions are as follows:
 ├── 2507-smallest-value-after-replacing-with-sum-of-prime-factors.ts
 ├── 2508-add-edges-to-make-degrees-of-all-nodes-even.ts
 ├── 2509-cycle-length-queries-in-a-tree.ts
+├── 2553-separate-the-digits-in-an-array.ts
 ├── 2618-check-if-object-instance-of-class.js
 ├── 2619-array-prototype-last.js
 ├── 2620-counter.js
